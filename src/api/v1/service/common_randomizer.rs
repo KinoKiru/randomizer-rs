17a17bed8d3d3b9a8637878a4f1eeac34e175228
Kinokiru <youriboulanger@gmail.com>
2023-07-15 10:57:58 +0200

    added full commen controller

--- src/api/v1/service/common_randomizer.rs
+++ src/api/v1/service/common_randomizer.rs
@@ -1,11 +1,13 @@
-use async_std::fs;
-use async_std::path::Path;
-use async_std::stream::StreamExt;
+use std::fs;
+use std::path::Path;
+
 use rand::{thread_rng, Rng};
 use tonic::{Request, Response, Status};
 
 use crate::proto::common_service_server::CommonService;
-use crate::proto::{CardRequest, CardResponse};
+use crate::proto::{
+    CardRequest, CardResponse, DateResponse, Location, SeasonResponse, TimeResponse,
+};
 
 #[derive(Debug, Default)]
 pub struct RandomCommonController {}
@@ -20,34 +22,42 @@ impl CommonService for RandomCommonController {
         let req = req.get_ref();
 
         let dir = fs::read_dir(Path::new("resource/cards"))
-            .await
             .map_err(|e| Status::internal(e.to_string()))?;
-        let files = Vec::new();
+        let files = dir.collect::<Vec<_>>();
 
-        while let Some(Ok(child)) = dir.next().await {
-            if child.metadata().await?.is_file() {
-                files.push(child);
-            } else {
-                info!("random card while got into a dir");
-            }
-        }
         let mut rng: rand::rngs::ThreadRng = thread_rng();
-        let mut card = &files[rng.gen_range(0..files.len())];
+        let mut card = files[rng.gen_range(0..files.len())].as_ref();
 
         // If joker card is not allowed loop until other is found
         if !req.allow_joker {
             // While file name contains joker get new card
             while matches!(
-                card.file_name().to_str(),
+                card.expect("Card should exist").file_name().to_str(),
                 Some(file_name) if file_name.contains("joker"))
             {
                 rng = thread_rng();
-                card = &files[rng.gen_range(0..files.len())]
+                card = files[rng.gen_range(0..files.len())].as_ref()
             }
         }
 
-        let image = fs::read(card.path()).await?;
+        let image = fs::read(card.unwrap().path()).map_err(|e| Status::internal(e.to_string()))?;
 
         Ok(Response::new(CardResponse { card: image }))
+    }
+
+    async fn get_random_time(&self, _: Request<()>) -> Result<Response<TimeResponse>, Status> {
+        todo!()
+    }
+
+    async fn get_random_season(&self, _: Request<()>) -> Result<Response<SeasonResponse>, Status> {
+        todo!()
+    }
+
+    async fn get_random_location(&self, _: Request<()>) -> Result<Response<Location>, Status> {
+        todo!()
+    }
+
+    async fn get_random_date(&self, _: Request<()>) -> Result<Response<DateResponse>, Status> {
+        todo!()
     }
 }
